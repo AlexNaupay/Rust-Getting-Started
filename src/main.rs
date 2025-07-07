f599544463be0c1d23c5be585c3c31147d49776f
Alex Naupay <alexnaupay@gmail.com>
2025-07-06 23:16:22 -0500

    feat(main): add variables and mutable example in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let name = "John";
+    let age:u8 = 25;  // Inmutable
+    let mut age_mut = 25;  // Mut = mutable
+    age_mut += 1;
+
+    println!("My name is {} and I am {} years old.", name, age);
+    println!("My name is {} and I am {} years old.", name, age_mut);
 }
